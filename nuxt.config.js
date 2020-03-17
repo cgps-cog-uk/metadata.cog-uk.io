6693c190c9abd764358856e34ecce0bc21105c31
Khalil <ka10@sanger.ac.uk>
2020-03-17 17:07:31 +0000

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,25 +2,25 @@
 import apiServerMiddleware from "./api";
 
 export default {
-  mode: 'universal',
+  mode: "universal",
   /*
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: "COG-UK",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: process.env.npm_package_description || "" },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+    ],
   },
   /*
   ** Customize the progress-bar color
   */
-  loading: { color: '#fff' },
+  loading: { color: "#fff" },
   /*
   ** Global CSS
   */
@@ -36,14 +36,14 @@ export default {
   */
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module'
+    "@nuxtjs/eslint-module",
   ],
   /*
   ** Nuxt.js modules
   */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios'
+    "@nuxtjs/axios",
   ],
   /*
   ** Axios module configuration
@@ -59,8 +59,8 @@ export default {
     ** You can extend webpack config here
     */
     extend (config, ctx) {
-    }
+    },
   },
 
   serverMiddleware: [apiServerMiddleware],
-}
+};
