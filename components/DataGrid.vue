aa0b9eec9e0a5910119ad54a3eaafaccb3034609
Khalil <ka10@sanger.ac.uk>
2020-03-20 20:41:31 +0000

    Add scrollbars to data table.
    Closes #1.

--- components/DataGrid.vue
+++ components/DataGrid.vue
@@ -91,6 +91,7 @@ export default {
   box-shadow: 0 12px 18px 2px rgba(34,0,51,.04),0 6px 22px 4px rgba(7,48,114,.12),0 6px 10px -4px rgba(14,13,26,.12);
   padding: 8px;
   border: 0 solid #d7d7db;
+  overflow: auto;
 }
 section {
   overflow: auto;
