5b4330f827f59fd34514b8e6c185a8d4fb54965b
Khalil <ka10@sanger.ac.uk>
2020-03-27 14:42:18 +0000

    WIP

--- api/routes/data/submit.js
+++ api/routes/data/submit.js
@@ -1,14 +1,47 @@
 /* eslint "no-throw-literal": 0 */
 
-const moment = require("moment");
-
 const climbApi = require("../../utils/climb-api");
+
+async function submitData(username, token, data) {
+  const [ sample ] = data;
+
+  let success;
+  const messages = {};
+
+  const biosamplesResults = await climbApi.createOrUpdateBiosamples(username, token, data);
+  success = biosamplesResults.success;
+  if (biosamplesResults.messages.length) {
+    Object.assign(messages, biosamplesResults.messages[0]);
+  }
+
+  if (biosamplesResults.success) {
+    if (sample.library_name) {
+      const libraryResults = await climbApi.createOrUpdateLibrary(username, token, data);
+      success = success && libraryResults.success;
+      if (libraryResults.messages.length) {
+        Object.assign(messages, libraryResults.messages[0]);
+      }
+    }
+    if (sample.run_name) {
+      const sequencingResults = await climbApi.createOrUpdateSequencing(username, token, data);
+      success = success && sequencingResults.success;
+      if (sequencingResults.messages.length) {
+        Object.assign(messages, sequencingResults.messages[0]);
+      }
+    }
+  }
+
+  return {
+    success,
+    messages: [ messages ],
+  };
+}
 
 module.exports = function (req, res, next) {
   console.info("Got request to create or update biosample data");
 
   Promise.resolve(req.body)
-    .then((data) => climbApi.createOrUpdateBiosamples(data.username, data.token, data.biosamples))
+    .then((data) => submitData(data.username, data.token, data.biosamples))
     .then((results) => {
       res.send({ ok: results.success, messages: results.messages });
     })
--- api/utils/climb-api.js
+++ api/utils/climb-api.js
@@ -3,7 +3,7 @@
 const axios = require("axios");
 
 const axiosClient = axios.create({
-  baseURL: "https://covid.majora.ironowl.it/api/v2/artifact",
+  baseURL: "https://covid.majora.ironowl.it/",
   headers: {
     "Content-Type": "application/vnd.api+json",
   },
@@ -12,7 +12,7 @@ const axiosClient = axios.create({
 module.exports.createOrUpdateBiosamples = async function (username, token, biosamples) {
   const response = await axiosClient.request({
     method: "POST",
-    url: "biosample/add/",
+    url: "api/v2/artifact/biosample/add/",
     data: {
       username,
       token,
@@ -25,22 +25,27 @@ module.exports.createOrUpdateBiosamples = async function (username, token, biosa
 module.exports.createOrUpdateLibrary = async function (username, token, library) {
   const response = await axiosClient.request({
     method: "POST",
-    url: "library/add/",
+    url: "api/v2/artifact/library/add/",
     data: {
       username,
       token,
-      library,
+      ...library[0],
+      biosamples: library,
     },
   });
-}
+  return response.data;
+};
+
 module.exports.createOrUpdateSequencing = async function (username, token, sequencing) {
   const response = await axiosClient.request({
     method: "POST",
-    url: "sequencing/add/",
+    url: "api/v2/process/sequencing/add/",
     data: {
       username,
       token,
-      sequencing,
+      ...sequencing[0],
+      runs: sequencing,
     },
   });
-}
+  return response.data;
+};
