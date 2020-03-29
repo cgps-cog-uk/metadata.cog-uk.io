8776ce0cc6f1d33f737ca3ee577984119ff9410d
Khalil <ka10@sanger.ac.uk>
2020-03-29 15:06:04 +0100

    Update date.vue

--- components/InputControl/date.vue
+++ components/InputControl/date.vue
@@ -5,18 +5,22 @@
     width="290px"
   >
     <template v-slot:activator="{ on }">
-      <v-text-field
-        v-bind:hide-details="inline ? true : false"
-        v-bind:hint="inline ? undefined : description"
-        v-bind:label="label"
-        v-bind:name="name"
-        v-bind:required="required"
-        v-bind:rules="rules"
-        v-bind:value="value"
-        outlined
-        readonly
-        v-on="on"
-      />
+      <div
+        v-on:click="handleClick"
+      >
+        <v-text-field
+          v-bind:hide-details="inline ? true : false"
+          v-bind:hint="inline ? undefined : description"
+          v-bind:label="label"
+          v-bind:name="name"
+          v-bind:required="required"
+          v-bind:rules="rules"
+          v-bind:value="value"
+          outlined
+          readonly
+          v-on="on"
+        />
+      </div>
     </template>
     <v-date-picker
       v-bind:value="value"
@@ -53,6 +57,9 @@ export default {
     };
   },
   methods: {
+    handleClick($event) {
+      this.modal = true;
+    },
     handleInput($event) {
       this.$emit("input", $event);
       this.modal = false;
